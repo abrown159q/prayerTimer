cf86ed4d5af6a96ef459934f2a0b2deab11d93b1
abrown159q <abrown159q@gmail.com>
2023-01-08 15:43:36 -0500

    Added in minutesButton flipping to view and hide minutes, as well as the getMinutes

--- testingPrayerTimer/script.js
+++ testingPrayerTimer/script.js
@@ -1,9 +1,46 @@
 
+//Time variables
+var timeInSeconds = 59;
 
+function getTimeInMinutes() {
+	return Math.floor(timeInSeconds / 60) + 1;
+	
+}
+//Start button on title screen
 const startButton = document.getElementById('startButton');
+const titlePanel = document.getElementById("titlePanel");
 
 startButton.addEventListener("click", hideTitlePanel);
-
 function hideTitlePanel() {
-	document.getElementById("titlePanel").style.display = 'none';
+	titlePanel.style.display = 'none';
+}
+
+
+//View minutes button toggle
+const minuteButton = document.getElementById('minuteButton');
+minuteButton.addEventListener("click",showMinutes);
+var showMinutesPressed = false;
+function showMinutes() {
+	if(!showMinutesPressed)
+	{
+		createMinutesLabel();
+	}
+	else
+	{
+		minuteButton.innerHTML = "Show minutes";
+	}
+	showMinutesPressed = !showMinutesPressed;
+}
+function createMinutesLabel(){
+			//TODO Make this look nicer
+		minuteButton.innerHTML = "Hide &nbsp&nbsp&nbsp&nbsp&nbsp" + getTimeInMinutes() 
+		+ "&nbsp&nbsp&nbsp from view";
+}
+
+
+var slider = document.getElementById("minuteSlider");
+slider.oninput = function() {
+	timeInSeconds = this.value * 60;
+	if(showMinutesPressed)
+		createMinutesLabel();
 }
