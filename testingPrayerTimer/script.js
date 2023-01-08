a74b4868612706b46bf599d82d2290cadc3108cc
abrown159q <abrown159q@gmail.com>
2023-01-08 16:22:33 -0500

    Pause Continue button working!

--- testingPrayerTimer/script.js
+++ testingPrayerTimer/script.js
@@ -4,19 +4,76 @@ var timeInSeconds = 59;
 
 function getTimeInMinutes() {
 	return Math.floor(timeInSeconds / 60) + 1;
-	
 }
+
+function decreaseTime() {
+	timeInSeconds = timeInSeconds - 1;
+}
+
+
+//Panels
+const titlePanel = document.getElementById("titlePanel");
+const startPanel = document.getElementById("runPanel");
+
+
+
+//******************************************************
 //Start button on title screen
 const startButton = document.getElementById('startButton');
-const titlePanel = document.getElementById("titlePanel");
+startButton.addEventListener("click", startButtonCommands);
+function startButtonCommands() {
+	hideTitlePanel();
+	showRunPanel();
+	startTimer();
+}
 
-startButton.addEventListener("click", hideTitlePanel);
 function hideTitlePanel() {
 	titlePanel.style.display = 'none';
 }
 
+function showRunPanel() {
+	runPanel.style.display = 'block';
+}
+let countdownInterval = null;
+function startTimer() {
+	 countdownInterval = setInterval(countdownTimer, 1000);
+}
 
-//View minutes button toggle
+function countdownTimer() {
+	document.getElementById("currentMinutesLabel").innerHTML = timeInSeconds; //getTimeInMinutes();
+	// document.getElementById("currentMinutesLabel").innerHTML = getTimeInMinutes();
+	//timeInSeconds = timeInSeconds - 1;
+	decreaseTime();
+}
+//********************************************************
+
+
+//********************************************************
+//Pause / continue button behavior
+const pauseContinueButton = document.getElementById('pauseContinueButton');
+pauseContinueButton.addEventListener("click", pauseContinueButtonCommands);
+var onPauseContinueButton = true;
+function pauseContinueButtonCommands() {
+	if(onPauseContinueButton)
+	{	
+		clearInterval(countdownInterval);
+		pauseContinueButton.innerHTML = "Continue";
+	}
+	else
+	{
+		countdownInterval = setInterval(countdownTimer, 1000);
+		pauseContinueButton.innerHTML = "Pause";
+	}
+	onPauseContinueButton = !onPauseContinueButton;
+
+}
+
+
+//********************************************************
+
+
+//********************************************************
+//View minutes button toggling to display minutes selected
 const minuteButton = document.getElementById('minuteButton');
 minuteButton.addEventListener("click",showMinutes);
 var showMinutesPressed = false;
@@ -36,11 +93,19 @@ function createMinutesLabel(){
 		minuteButton.innerHTML = "Hide &nbsp&nbsp&nbsp&nbsp&nbsp" + getTimeInMinutes() 
 		+ "&nbsp&nbsp&nbsp from view";
 }
+//********************************************************
 
 
+
+//********************************************************
+//Dynamic range slider action
 var slider = document.getElementById("minuteSlider");
 slider.oninput = function() {
 	timeInSeconds = this.value * 60;
 	if(showMinutesPressed)
 		createMinutesLabel();
 }
+//********************************************************
+
+
+//
